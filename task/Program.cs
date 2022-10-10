d39a90fbc6acff12b1318bc157a5b29aaa23c449
A.Islamshin <arshavin0511@gmail.com>
2022-10-10 18:29:05 +0300

    task

---
+++ task/Program.cs
@@ -0,0 +1,13 @@
+﻿string [] array = {"hello", "23", "yes", "war", "1234455"};
+void Massiv (string [] arr)
+{
+    for (int i= 0; i < arr.Length; i++)
+    {
+        int count = arr[i].Count();
+        if (count <= 3)
+        {
+            Console.WriteLine(arr[i]);
+        }
+    }
+}
+Massiv(array);
