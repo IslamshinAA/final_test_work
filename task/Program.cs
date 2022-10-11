7d44233351a8fd362ad6570428900450494dcb13
A.Islamshin <arshavin0511@gmail.com>
2022-10-11 12:35:27 +0300

    readme

--- task/Program.cs
+++ task/Program.cs
@@ -1,13 +1,15 @@
-﻿string [] array = {"hello", "23", "yes", "war", "1234455"};
+﻿string [] array = {"hello", ":)", "yes", "not", "1234455"};
 void Massiv (string [] arr)
 {
+    Console.Write("[");
     for (int i= 0; i < arr.Length; i++)
     {
         int count = arr[i].Count();
         if (count <= 3)
-        {
-            Console.WriteLine(arr[i]);
-        }
+        {   
+            Console.Write($" \"{arr[i]}\" ");
+        }  
     }
+    Console.Write("]");
 }
 Massiv(array);
